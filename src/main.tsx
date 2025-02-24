79ab7caa5f5e5c3a331764abe2ca2156b5850295
Adrian <adriannugrohobasuki@gmail.com>
2025-02-24 20:58:11 +0700

    Some elements to components

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Homepage from './Auth/signup'
+// import Homepage from './Auth/signup'
+import Homepage from './Homepage/homepage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
